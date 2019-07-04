27fee926bf3362231e6a32cfa6a63484506a1c64
Daniel Millen <dan@dansclan.com>
2019-07-04 18:03:44 -0400

    Add FoV and remember explored tiles

--- engine.py
+++ engine.py
@@ -1,66 +1,85 @@
 import tcod as libtcod
 
 from entity import Entity
+from fov_functions import initialize_fov, recompute_fov
 from input_handlers import handle_keys
 from map_objects.game_map import GameMap
 from render_functions import render_all, clear_all
 
 
 def main():
-	screen_width = 80
-	screen_height = 50
-	map_width = 80
-	map_height = 45
+    screen_width = 80
+    screen_height = 50
+    map_width = 80
+    map_height = 45
 
-	room_max_size = 10
-	room_min_size = 6
-	max_rooms = 30
+    room_max_size = 10
+    room_min_size = 6
+    max_rooms = 30
 
-	colors = {
-		'dark_wall': libtcod.Color(0, 0, 100),
-		'dark_ground': libtcod.Color(50, 50, 150)
-	}
+    fov_algorighm = 0
+    fov_light_walls = True
+    fov_radius = 10
 
-	player = Entity(int(screen_width/2), int(screen_height/2), '@', libtcod.white)
-	npc = Entity(int(screen_width/2-5), int(screen_height/2), '@', libtcod.yellow)
-	entities = [npc, player]
+    colors = {
+            'dark_wall':    libtcod.Color(0, 0, 100),
+            'dark_ground':  libtcod.Color(50, 50, 150),
+            'light_wall':   libtcod.Color(130, 110, 50),
+            'light_ground': libtcod.Color(200, 180, 50)
+    }
 
-	libtcod.console_set_custom_font('arial10x10.png', libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
+    player = Entity(int(screen_width / 2), int(screen_height / 2), '@', libtcod.white, True)
+    npc = Entity(int(screen_width / 2 - 5), int(screen_height / 2), '@', libtcod.yellow)
+    entities = [npc, player]
 
-	libtcod.console_init_root(screen_width, screen_height, 'libtcod tutorial revised', False)
+    libtcod.console_set_custom_font('arial10x10.png', libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
 
-	con = libtcod.console_new(screen_width, screen_height)
+    libtcod.console_init_root(screen_width, screen_height, 'libtcod tutorial revised', False)
 
-	game_map = GameMap(map_width, map_height)
-	game_map.make_map(max_rooms, room_min_size, room_max_size, map_width, map_height, player)
+    con = libtcod.console_new(screen_width, screen_height)
 
-	key = libtcod.Key()
-	mouse = libtcod.Mouse()
+    game_map = GameMap(map_width, map_height)
+    game_map.make_map(max_rooms, room_min_size, room_max_size, map_width, map_height, player)
 
-	while not libtcod.console_is_window_closed():
-		libtcod.sys_check_for_event(libtcod.EVENT_KEY_PRESS, key, mouse)
+    fov_recompute = True
 
-		render_all(con, entities, game_map, screen_width, screen_height, colors)
-		libtcod.console_flush()
+    fov_map = initialize_fov(game_map)
 
-		clear_all(con, entities)
+    key = libtcod.Key()
+    mouse = libtcod.Mouse()
 
-		action = handle_keys(key)
-		move = action.get('move')
-		exit = action.get('exit')
-		fullscreen = action.get('fullscreen')
+    while not libtcod.console_is_window_closed():
+        libtcod.sys_check_for_event(libtcod.EVENT_KEY_PRESS, key, mouse)
 
-		if move:
-			dx, dy = move
-			if not game_map.is_blocked(player.x + dx, player.y + dy):
-				player.move(dx, dy)
+        render_all(con, entities, game_map, fov_map, fov_recompute, screen_width, screen_height, colors)
 
-		if exit:
-			return True
+        fov_recompute = False
 
-		if fullscreen:
-			libtcod.console_set_fullscreen(not libtcod.console_is_fullscreen())
+        libtcod.console_flush()
+
+        clear_all(con, entities)
+
+        action = handle_keys(key)
+        move = action.get('move')
+        exit = action.get('exit')
+        fullscreen = action.get('fullscreen')
+
+        if move:
+            dx, dy = move
+            if not game_map.is_blocked(player.x + dx, player.y + dy):
+                player.move(dx, dy)
+
+                fov_recompute = True
+
+        if exit:
+            return True
+
+        if fullscreen:
+            libtcod.console_set_fullscreen(not libtcod.console_is_fullscreen())
+
+        if fov_recompute:
+            recompute_fov(fov_map, player.x, player.y, fov_radius, fov_light_walls, fov_algorighm)
 
 
 if __name__ == '__main__':
-	main()
+    main()
--- entity.py
+++ entity.py
@@ -2,11 +2,12 @@ class Entity:
     """
     A generic object to represent players, items, enemies, etc.
     """
-    def __init__(self, x, y, char, color):
+    def __init__(self, x, y, char, color, player=False):
         self.x = x
         self.y = y
         self.char = char
         self.color = color
+        self.player = player
 
     def move(self, dx, dy):
         # Move the entity by the given amount
